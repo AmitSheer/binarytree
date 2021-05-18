b67c2ef7a0806e9dcfb36470e46d592ddcdaf2dd
AmitSheer <amitcohen95@gmail.com>
2021-05-18 11:55:16 +0300

    added init test file

---
+++ Test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by amit on 18/05/2021.
+//
+
+//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <string>
+#include <unistd.h>
+#include <algorithm>
+using namespace std;
+using namespace doctest;
